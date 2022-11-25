04949349b6fc2d789baa57abf376d02718ac10b9
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2022-11-25 23:58:30 +0000

    groups and student classes working

---
+++ groups.py
@@ -0,0 +1,13 @@
+class groups:
+    def __init__(self, group_number):
+        self.students = []
+        self.group_number = group_number
+
+    def get_students(self):
+        return self.students
+
+    def add_students(self, student):
+        self.students.append(student)
+
+    def group_size(self):
+        return len(self.students)
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import csv
 import pandas as pd
 import numpy as np
 import random
+from students import student
+from groups import groups
 
 
 def display_csv(path):
@@ -38,15 +40,43 @@ def initialize(csv_input, group_size):
     number_of_groups = int(np.floor(len(csv_input) / group_size))
     # print(number_of_groups)
     total = 0
-    random.shuffle(csv_input)
-    #print(csv_input)
+    # random.shuffle(csv_input)
+    # print(csv_input)
+    output_groups = []
+    for i in range(1, number_of_groups + 1):
+        output_groups.append(groups(i))
     while total != len(csv_input):
         for i in range(1, number_of_groups + 1):
             if total != len(csv_input):
-                #print(csv_input[total])
+                # print(csv_input[total])
                 (csv_input[total])["Group"] = i
+                output_groups[i - 1].add_students(student(csv_input[total]["Names"], (csv_input[total])["Age"]))
+
                 total += 1
-    return csv_input
+
+    return output_groups
+
+
+def csv_to_students(csv_input):
+    students = []
+    for i in csv_input:
+        students.append(student(i["Names"], i["Age"]))
+    return students
+
+
+def students_to_csv(students_object):
+    students = []
+    for i in students_object:
+        students.append({"Names": i.name, "Age": i.age})
+    return students
+
+
+def groups_to_students(groups_array):
+    for i in groups_array:
+        print("group number: ", i.group_number)
+        print("group size: ", i.group_size())
+        print("students: ", (students_to_csv(i.get_students())))
+        print("")
 
 
 # Press the green button in the gutter to run the script.
@@ -56,5 +86,10 @@ if __name__ == '__main__':
     # extract_csv_data(get_csv(data_path))
     # print(get_csv(data_path))
     # save_csv(get_csv(data_path))
-    initialized_groups = initialize(csv_input, 5)
-    save_csv(initialized_groups)
+    initialized_groups = initialize(csv_input, 2)
+    groups_to_students(initialized_groups)
+    # save_csv(initialized_groups)
+    # students = csv_to_students(csv_input)
+    # print(students)
+    # print(csv_input)
+    # print(students_to_csv(students))
