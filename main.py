37016111cf9e10987481a67e2ec935a84d6a5906
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2022-11-25 22:41:59 +0000

    working groups basic

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import csv
 import pandas as pd
-
-
+import numpy as np
+import random
 
 
 def display_csv(path):
@@ -27,17 +27,34 @@ def save_csv(csv_input):
         dict_writer.writeheader()
         dict_writer.writerows(csv_input)
 
+
 def extract_csv_data(csv_input):
-    #print(csv_input)
+    # print(csv_input)
     for i in csv_input:
         print(i["Names"])
+
+
+def initialize(csv_input, group_size):
+    number_of_groups = int(np.floor(len(csv_input) / group_size))
+    # print(number_of_groups)
+    total = 0
+    random.shuffle(csv_input)
+    #print(csv_input)
+    while total != len(csv_input):
+        for i in range(1, number_of_groups + 1):
+            if total != len(csv_input):
+                #print(csv_input[total])
+                (csv_input[total])["Group"] = i
+                total += 1
+    return csv_input
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data_path = "test_data\data.csv"
-    #extract_csv_data(get_csv(data_path))
-    #print(get_csv(data_path))
-
-
-    save_csv(get_csv(data_path))
+    csv_input = get_csv(data_path)
+    # extract_csv_data(get_csv(data_path))
+    # print(get_csv(data_path))
+    # save_csv(get_csv(data_path))
+    initialized_groups = initialize(csv_input, 5)
+    save_csv(initialized_groups)
