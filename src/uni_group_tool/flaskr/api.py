5ea310e08da0856ad7db502a823ca9ef19d148bf
Dauthlin <70644516+Dauthlin@users.noreply.github.com>
2022-12-14 13:55:09 +0000

    flask with json api done

--- src/uni_group_tool/flaskr/api.py
+++ src/uni_group_tool/flaskr/api.py
@@ -1,21 +1,23 @@
-import flask
-
+from flask import Flask, request , jsonify
 from uni_group_tool.main import groups_to_csv, run
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
 # http://192.168.11.61:5000
-@app.route('/run_program', methods=['GET'])
+# curl -i -H "Content-Type: application/json" -X POST -d '{"size_of_teams":8,"shuffle":true,"criteria":{"diversity":["average","gender"],"amount_to_be_together":[["gender","F",2],["home","O",2]],"specific_teams":[[["208026943",3],["208063956",3],["207069131",4]]]},"weights":{},"data_path":"test_data/sample.csv","debugging":false,"saving":true}' http://192.168.11.61:5000/run_program
+@app.route('/run_program', methods=['GET','POST'])
 def run_program():
-    criteria = {"diversity": ["average", "gender"],
-                "amount_to_be_together": [("gender", "F", 2), ("home", "O", 2)],
-                "specific_teams": [[("208026943", 3), ("208063956", 3), ("207069131", 4)]]}
-    size_of_teams = 6
-    shuffle = True
-    weights = {}  # type: dict[str,int]
-    data_path = "test_data/sample.csv"
-    debugging = False
-    saving = True
+    data = request.json
+    # parsing data
+    size_of_teams = data["size_of_teams"]
+    criteria = data["criteria"]
+    shuffle = data["shuffle"]
+    weights = data["weights"]
+    data_path = data["data_path"]
+    debugging = data["debugging"]
+    saving = data["saving"]
+    # run program
     best_team = run(criteria, size_of_teams, shuffle, weights, data_path, debugging,saving)
     return groups_to_csv(best_team)
 
